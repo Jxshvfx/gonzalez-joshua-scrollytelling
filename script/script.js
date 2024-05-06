d264a79de244c745802047e718bf04f3ac7c706c
Jxshvfx <spiteyhd@gmail.com>
2024-05-06 14:07:35 -0400

    premiere_animations

--- script/script.js
+++ script/script.js
@@ -12,9 +12,6 @@ gsap.to('#feuille-2', {
     x: "150vw", repeat: -1, duration: 5, rotation: 180
 })
 
-gsap.to('#dinosaure', {
-    x: "5vw", repeat: -1, duration: 5,
-})
 
 gsap.to('#grand_pere', {
     x: "5vw", repeat: -1, duration: 5,
@@ -32,9 +29,7 @@ gsap.to('#ours', {
     x: "-5vw", repeat: -1, duration: 5,
 })
 
-gsap.to('.jack', {
-    x: "5vw", repeat: -1, duration: 5, opacity: 1
-})
+
 
 timeline.to('.fleche-01', {
     opacity: 1,
@@ -63,3 +58,31 @@ timeline.to('.fleche-03', {
     delay: 0.1
 });
 
+let chapter1 = gsap.timeline({
+    scrollTrigger: {
+        markers: true,
+        scrub: true,
+        end: 'bottom top',
+        pin: true,
+        trigger: "#chap-1, .jack, #chap-titre-1, .micro-big, #micro-text-1, #micro-text-2, #cercle-transition"
+    }
+})
+.to("#chap-1", {opacity: 1, duration: 5})
+.to("#chap-titre-1", {opacity: 1, duration: 10})
+.to('.jack', {x: "5vw", duration: 5, opacity: 1})
+.to('.micro-big', {scale: 1, duration: 5})
+.to('#micro-text-1', {duration: 5, opacity: 1})
+.to('#micro-text-1', {duration: 5, opacity: 0})
+.to('#micro-text-2', {duration: 5, opacity: 1})
+.to('#cercle-transition', {scale: 12, duration: 10});
+
+let chapter2 = gsap.timeline({
+    scrollTrigger: {
+        markers: true,
+        scrub: true,
+        pin: true,
+        trigger: "#chap-2, #cercle-transition-2"
+    }
+})
+.to('#cercle-transition-2', {scale: 0, duration: 10})
+.to('#dinosaure', { x: "5vw", opacity: 1, duration: 5,})
